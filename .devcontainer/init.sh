a073b492ab6b914644b89db63e62ff73763abd24
raffa <zus6wyq9v@mozmail.com>
2025-06-19 05:47:35 +0000

    @feat: Added python debugger for django debugging. Please change the file if ever being used.

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -11,7 +11,4 @@ sudo apt update -y
 echo "Upgrading packages...."
 sudo apt upgrade -y
 
-echo "Installing global npm packages (expo, ngrok)..."
-npm install -g expo ngrok
-
 echo "Dev container initial setup complete!"
